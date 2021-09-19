945f8afc04337d253e92483ce2f8ced85c95f990
Andrei Rybak <rybak.a.v@gmail.com>
2021-09-19 17:41:13 +0200

    Gradle: upgrade to Kotlin 1.5.30

--- either-kotlin/build.gradle.kts
+++ either-kotlin/build.gradle.kts
@@ -17,7 +17,7 @@
  * along with Either-JVM.  If not, see <https://www.gnu.org/licenses/>.
  */
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.30"
 }
 
 group = "dev.andrybak"
